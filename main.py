c787cae1989144042f8c3528b2fc724eac9e55fc
Evanderss <evanderramirez150@gmail.com>
2022-12-02 23:07:43 -0600

    creando el modelo y el response body

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+#python
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+#fastapi
+from fastapi import FastAPI, Body
+
 
 app = FastAPI()
+
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 #Path operations "decorator"
 @app.get("/")
 def home():
     return {"hello": "World"}
+
+
+#Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
