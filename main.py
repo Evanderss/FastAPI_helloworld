3a34973dec012ab9819aaa9ee3940597ad0f47f6
Evanderss <evanderramirez150@gmail.com>
2022-12-05 16:42:48 -0600

    terminando validaciones

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ class Location(BaseModel):
 
 
 class Person(BaseModel):
-    first_name: str = Field(..., min_length=0, max_length=50)
-    last_name: str = Field(..., min_length=0, max_length=50)
-    age: int = Field(..., gt=0, le=115)
-    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
-    is_married: Optional[bool] = Field(default=None)
+    first_name: str = Field(..., min_length=0, max_length=50, example="Miguel")
+    last_name: str = Field(..., min_length=0, max_length=50, example="Torres")
+    age: int = Field(..., gt=0, le=115, example=25)
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
 
 
 #Path operations "decorator"
@@ -48,23 +48,23 @@ def create_person(person: Person = Body(...)):
 #Validations: Query Parameters
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50, title="Person Name",description="This is the person name. It's between 1 and 50 characters"),
-    age: str = Query(..., title="Person Age", description="This is the person age. It's required")): 
+    name: Optional[str] = Query(None, min_length=1, max_length=50, title="Person Name",description="This is the person name. It's between 1 and 50 characters", example="Rocío"),
+    age: str = Query(..., title="Person Age", description="This is the person age. It's required", example=25)): 
     return {name: age}
 
 
 #Validations: Path Parameters
 @app.get("/person/detail/{person_id}")
 def show_person(
-    person_id: int = Path(..., gt=0)): 
+    person_id: int = Path(..., gt=0, example=123)): 
     return {person_id: "It exists!"}
 
 
 #Validations: Request Body
 @app.put("/person/{person_id}")
-def update_person(person_id: int = Path(..., title="Person ID", description="This is the person ID", gt=0),
-    person: Person = Body(...),
-    location: Location = Body(...)): 
-    results = person.dict()
-    results.update(location.dict())
-    return results
+def update_person(person_id: int = Path(..., title="Person ID", description="This is the person ID", gt=0, example=123),
+    person: Person = Body(...)): 
+    #location: Location = Body(...)
+    #results = person.dict()
+    #results.update(location.dict())
+    return person
